674228d80f3bfb17262fd4480e4beaf5d26c0111
Yael <yaeljuego@live.com.mx>
2021-09-16 23:59:28 -0700

    Gawr Gura Animation
    Added the animation, fps bug not fiixed

--- rules.mk
+++ rules.mk
@@ -1,7 +1,7 @@
 # -----
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
+RGBLIGHT_ENABLE = no       # Enable keyboard RGB underglow
 
 ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 
